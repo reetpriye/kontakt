978ac7989bab681cac43c308b77dcd89a7d14141
Reet Priye <reetpriye@gmail.com>
2021-01-12 10:27:49 +0530

    Fix font weight issue while showing alerts

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -9,7 +9,7 @@ const Alerts = () => {
     alerts.length > 0 &&
     alerts.map(alert => (
       <div key={alert.id} className={`alert alert-${alert.type}`}>
-        <i className='fas fa-info-circle'>{alert.msg}</i>
+        <i className='fas fa-info-circle' /> {` ${alert.msg}`}
       </div>
     ))
   );
